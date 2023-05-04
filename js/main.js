64ae3494136fca744449a05249aaff7b79b4c607
Javohir <shuhratovjavohir2022@gmail.com>
2023-05-05 00:37:14 +0500

    second commit

--- js/main.js
+++ js/main.js
@@ -5,11 +5,36 @@ const $burger1 = document.querySelector('.burger-1')
 const $burger2 = document.querySelector('.burger-2')
 const $burger3 = document.querySelector('.burger-3')
 
+const $call = document.querySelector('#call')
+const $callBlock = document.querySelector('.callBlock')
+
+const $headerLeft = document.querySelector('.header__left')
+
+const $closeCall = document.querySelector('.call__circle-2')
+
+const $header = document.querySelector('.header')
+
 $burger.addEventListener('click', function(){
     $menu.classList.toggle('menu-active')
-    
+    $callBlock.classList.remove('callBlock-active')
+
     $burger1.classList.toggle('burger1-active')
     $burger2.classList.toggle('burger2-active')
     $burger3.classList.toggle('burger3-active')
     
+})
+
+
+
+
+
+$call.addEventListener('click', function(){
+    $menu.classList.remove('menu-active')
+    $callBlock.classList.toggle('callBlock-active')
+    $burger1.classList.remove('burger1-active')
+    $burger2.classList.remove('burger2-active')
+    $burger3.classList.remove('burger3-active')
+    $headerLeft.classList.toggle('header__left-none')
+    $closeCall.classList.toggle('.none')
+    $header.classList.toggle('header-end')
 })
