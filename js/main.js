f2ab6e8ea07187941064214c4285c3334aad15bd
Javohir <shuhratovjavohir2022@gmail.com>
2023-05-07 10:59:12 +0500

    add second page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 const $burger = document.querySelector('.burger')
 const $menu = document.querySelector('.menu')
-
 const $burger1 = document.querySelector('.burger-1')
 const $burger2 = document.querySelector('.burger-2')
 const $burger3 = document.querySelector('.burger-3')
-
-const $call = document.querySelector('#call')
+const $telCall = document.querySelector('.tel-call')
 const $callBlock = document.querySelector('.callBlock')
-
 const $headerLeft = document.querySelector('.header__left')
-
 const $closeCall = document.querySelector('.call__circle-2')
-
 const $header = document.querySelector('.header')
 
 $burger.addEventListener('click', function(){
@@ -24,11 +19,7 @@ $burger.addEventListener('click', function(){
     
 })
 
-
-
-
-
-$call.addEventListener('click', function(){
+$telCall.addEventListener('click', function(){
     $menu.classList.remove('menu-active')
     $callBlock.classList.toggle('callBlock-active')
     $burger1.classList.remove('burger1-active')
@@ -37,4 +28,18 @@ $call.addEventListener('click', function(){
     $headerLeft.classList.toggle('header__left-none')
     $closeCall.classList.toggle('.none')
     $header.classList.toggle('header-end')
+})
+
+
+/* section - 3 ========== 
+===============>>>>>>>>*/
+const $vip = document.querySelector('.vip')
+const $vipSubTitle = document.querySelector('#vip')
+
+$vip.addEventListener('mouseover', function(event){
+    $vipSubTitle.classList.add('none-active')
+})
+
+$vip.addEventListener('mouseout', function(){
+    $vipSubTitle.classList.remove('none-active')
 })
